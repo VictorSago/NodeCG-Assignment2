e6d92114f4afae81b85a022b19dfaf6e5a3841cb
Victor Sago <victor.sago@elstandard.se>
2022-10-19 14:15:20 +0200

    Second stage of the assignment complete

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const express = require("express");
 
 const app = express();
 
-app.use('/', (req, res, next) => {
+app.use('/users', (req, res, next) => {
   console.log("This is the first middleware.");
-  next();
+  let rstring = "<h2>You're on the \"users\" page</h2>"
+  rstring += "<p>To go to the Main page click on this <a href='/'>link</a>.</p>"
+  res.send(rstring);
 });
 
 app.use('/', (req, res, next) => {
   console.log("This is the second middleware.");
-  res.send('<h1>Express Server is working fine!</h1>');
+  let rstring = "<h2>You're on the \"Main\" page</h2>";
+  rstring += "<p>Go to the <a href='/users'>Users</a> page.</p>"
+  res.send(rstring);
 });
 
 app.listen(3000);
